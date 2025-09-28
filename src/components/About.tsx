7d197bcc830aa9863d9fb8ecddf93af5cd3daccb
codemuse-bcka777 <pantbisika159@gmail.com>
2025-09-28 15:06:01 +0545

    changed the pfp to avatar

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -140,7 +140,7 @@ const About = () => {
                 {/* Placeholder Content */}
                 <div className="relative z-10 h-full flex justify-center items-center">
                   <motion.img
-                    src="./profile-pic.png"
+                    src=".7/.png"
                     alt="Bisika Pant"
                     className="w-full h-full object-cover rounded-xl"
                     whileHover={{ scale: 1.05 }}
